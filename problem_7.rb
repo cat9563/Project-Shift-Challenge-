2c3970493c58dd8c503c461b0c69c1d1f1da0a5c
Crystal Ramirez <crystal9563@gmail.com>
2018-10-06 07:03:49 -0400

    more thoughts on how to solve #7

--- problem_7.rb
+++ problem_7.rb
@@ -2,11 +2,15 @@ cost = 154
 currency = [100, 20, 10, 5, 1] #100*1, 20*2, 10*1, 4*1
 num = []
 
-#I would need to create a loop here that takes the value of each element
-#it takes the cost divides it by 100 for elment 0 if there is a remainder
-# and its positve it moves to the next one if its false it says it how many
-#100 bills it needs to complete the transaction.
+#ideas for some logic
 
-#the problem I am running into is I cant seem to find the right syntac for this
-# I have looked at the answer to get an understanding of the problem
-# I just dont want to put that down as my own.
+if cost/currency[0] > 0
+  puts  #total amount of times it went through
+  # need to check for a remainder afterwards
+  if cost%currency[0] > 0
+    # need to move to check next element
+    # make it check to see to see the amount of times
+    #the remainder is divided
+    # repeat this loop until there
+    #is no remainder
+  end
