7e7c350d48dc8d3d6b3c9af7260c9a40534045e8
Crystal Ramirez <crystal9563@gmail.com>
2018-10-08 18:48:51 -0400

    solved #6

--- problem_6.rb
+++ problem_6.rb
@@ -1,6 +1,16 @@
 array1 = ['a', 'b', 'c', 'a', 'a', 'b', 'd']
 array2 = ['a', 'b', 'b', 'a', 'e', 'c', 'c', 'g']
+arr = []
 
-combination = array1 + array2
+arr1 = array1.sort
+arr2 = array2.sort
 
-print combination.sort_by(&:downcase)
+combo = arr1 + arr2
+arr3 = arr1 & arr2
+
+unique = combo.uniq
+common = arr3[0]*2 + arr3[1]*2 + arr3[2]*2
+
+arr4 = common + unique[3]*1 + unique[4]*1 + unique[5]*1
+
+puts arr.push(arr4)
